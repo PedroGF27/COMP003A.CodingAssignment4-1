9e822f7ba0638b309d6f00092c2f2958d6e50bc3
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-09 16:03:51 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Pedro Garcia
+// COMP-003A
+// Johnathan Cruz
+// Inventory management application with a minimum of 10 elements in the colection.
+namespace COMP003A.CodingAssignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
