9a9607f8ddd169df76c3af6a498c392e953ce213
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-09 16:05:13 -0800

    had to create another project because cloning didnt work for me

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -8,7 +8,57 @@ namespace COMP003A.CodingAssignment4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] products = new string[10];
+            int[] storage = new int[10];
+            int number = 0;
+
+            while (true)
+            {
+                Console.WriteLine("1. Add a product");
+                Console.WriteLine("2. Update Product Quantity");
+                Console.WriteLine("3. View Inventory Summary");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice: ");
+                number = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine();
+                }
+
+                number = 1;
+                if (number == 1)
+                {
+                    int index = Array.FindIndex(products, name => name == null);
+                    Console.WriteLine("Product name: ");
+                    products[index] = Console.ReadLine();
+
+                    Console.WriteLine("Product Quantity: ");
+                    try
+                    {
+                        storage[index] = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+                else if (number == 2)
+                {
+                    int index = Array.IndexOf(products);
+                }
+                else if (number == 3)
+                {
+                    for (int i = 0; i < storage.Length; i++)
+                    {
+                        Console.WriteLine($"");
+                    }
+                }
+            }
         }
     }
 }
